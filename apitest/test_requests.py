f0ce169bfddde8d6f628e10b43d22ae3a8c26f9f
Administrator <13823594530@163.com>
2020-12-15 21:04:31 +0800

    12.15接口测试代码

--- apitest/test_requests.py
+++ apitest/test_requests.py
@@ -1,6 +1,7 @@
 import requests
-
+import pytest
 class Testrequests():
+    @pytest.fixture()
     def test_token_get(self):
         r=requests.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=ww0a755c411005b357&corpsecret=-tzXl87I-yRulX0ikZQGqnJ4Th2qfBgpRa_1H3FcmG0")
         print(r.json()['access_token'])
